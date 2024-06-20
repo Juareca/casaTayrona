c721a7e1025279148a7377a33f305481e3ae249f
Juareca <alberjuan2411@gmail.com>
2024-06-20 14:57:53 -0500

    se quitó todo de ses aws y flask email

--- casatayronahostal.py
+++ casatayronahostal.py
@@ -1,15 +1,8 @@
+# app.py
+
 from flask import Flask, render_template, request
-from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Elnovato_#24'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'alberjuan2411@gmail.com'
-app.config['MAIL_PASSWORD'] = 'vhpb uvpm fqde ifxl'  # Ajusta tu contraseña aquí
-
-mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -25,24 +18,21 @@ def rodadero():
 
 @app.route('/formulario', methods=['GET', 'POST'])
 def formulario():
-    reserva_exitosa = None
-    
-    if request.method == 'POST':
-        destino = request.form['destino']
-        habitaciones = request.form['habitaciones']
-        huespedes = request.form['huespedes']
-        precio = request.form['precio']
-        nombre = request.form['nombre']
-        apellido = request.form['apellido']
-        email = request.form['email']
-        telefono = request.form['telefono']
-        cantidad_noches = request.form['cantidad-noches']
+    reserva_exitosa = False
 
-        # Envío de correo electrónico usando Flask-Mail
-        msg = Message('Reserva desde Casa Tayrona',
-                      sender='alberjuan2411@gmail.com',
-                      recipients=['alberjuan2411@gmail.com'])  # Cambia al destinatario deseado
-        msg.body = f'''Hola,
+    if request.method == 'POST':
+        destino = request.form.get('destino')
+        habitaciones = request.form.get('habitaciones')
+        huespedes = request.form.get('huespedes')
+        precio = request.form.get('precio')
+        nombre = request.form.get('nombre')
+        apellido = request.form.get('apellido')
+        email = request.form.get('email')
+        telefono = request.form.get('telefono')
+        cantidad_noches = request.form.get('cantidad-noches')
+
+        # Construir el cuerpo del mensaje
+        email_body = f'''Hola,
 
         Se ha recibido una nueva reserva desde el formulario de Casa Tayrona:
 
@@ -56,13 +46,10 @@ def formulario():
         Precio: {precio}
 
         Gracias.'''
-        
-        try:
-            mail.send(msg)
-            reserva_exitosa = True
-        except Exception as e:
-            print(str(e))  # Imprime el error para depuración
-            reserva_exitosa = False
+
+        # Aquí podrías realizar otras acciones relacionadas con el formulario, como guardar los datos en una base de datos o enviar notificaciones, etc.
+
+        reserva_exitosa = True  # Simplemente marcamos la reserva como exitosa para este ejemplo
 
     return render_template('formulario.html', reserva_exitosa=reserva_exitosa)
 
