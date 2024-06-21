4a15cda3ea345b901e08c2f02581387429cc79a1
Juareca <alberjuan2411@gmail.com>
2024-06-21 13:10:39 -0500

    Se agregó aws ses funcional :D

--- casatayronahostal.py
+++ casatayronahostal.py
@@ -1,8 +1,17 @@
-# app.py
-
 from flask import Flask, render_template, request
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
 
 app = Flask(__name__)
+
+# Configuración de SES (reemplaza con tus propias credenciales)
+smtp_username = "AKIA2UC27IZCBIS5ER7G"
+smtp_password = "BNXVglCvJkFqftPcEmKqENsg7eMxd1Lub3uWHbdp/QsL"
+smtp_server = "email-smtp.sa-east-1.amazonaws.com"
+smtp_port = 587
+smtp_verified_email = "alberjuan2411@gmail.com"
 
 @app.route('/')
 def index():
@@ -47,9 +56,26 @@ def formulario():
 
         Gracias.'''
 
-        # Aquí podrías realizar otras acciones relacionadas con el formulario, como guardar los datos en una base de datos o enviar notificaciones, etc.
+        # Enviar correo usando SES
+        try:
+            msg = MIMEMultipart()
+            msg['From'] = smtp_verified_email
+            msg['To'] = smtp_verified_email
+            msg['Subject'] = "Nueva reserva desde Casa Tayrona"
 
-        reserva_exitosa = True  # Simplemente marcamos la reserva como exitosa para este ejemplo
+            msg.attach(MIMEText(email_body, 'plain'))
+
+            server = smtplib.SMTP(smtp_server, smtp_port)
+            server.starttls()  # Iniciar TLS para encriptar la conexión
+            server.login(smtp_username, smtp_password)
+            server.sendmail(smtp_verified_email, smtp_verified_email, msg.as_string())
+            server.quit()
+
+            reserva_exitosa = True
+
+        except Exception as e:
+            print(f"Error al enviar el correo: {str(e)}")
+            reserva_exitosa = False
 
     return render_template('formulario.html', reserva_exitosa=reserva_exitosa)
 
